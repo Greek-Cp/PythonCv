fd3eb21858cc9699dca8bec5e25854f5a2ca967b
Yanuar Tr <82995911+Greek-Cp@users.noreply.github.com>
2023-10-01 01:30:50 -0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,73 @@
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QLabel, QMessageBox, QWidget, QGridLayout
+from PyQt5.QtGui import QPixmap, QImage
+import cv2
+import numpy as np
+import sys
+
+class Ui_MainWindow(QMainWindow):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__()
+        self.setupUi(self)
+        self.setStyleSheet("background-color: lightgrey;")
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1200,800)
+
+        grid_layout = QGridLayout()
+
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setLayout(grid_layout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.menubar = self.menuBar()
+
+        self.menuFile = self.menubar.addMenu("File")
+
+        self.actionOpen = self.menuFile.addAction("Open Image")
+        self.actionOpen.triggered.connect(self.open_image)
+
+        self.actionSave = self.menuFile.addAction("Save Image")
+        self.actionSave.triggered.connect(self.save_image)
+
+        self.beforeImageView = QLabel(self.centralwidget)
+        self.beforeImageView.setStyleSheet("border: 2px solid black;")
+        grid_layout.addWidget(self.beforeImageView, 0, 0)
+
+        self.afterImageView = QLabel(self.centralwidget)
+        self.afterImageView.setStyleSheet("border: 2px solid black;")
+        grid_layout.addWidget(self.afterImageView, 0, 1)
+
+    def open_image(self):
+        options = QFileDialog.Options()
+        filepath, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Images (*.png *.jpg *.jpeg)", options=options)
+        if filepath:
+            self.image = cv2.imread(filepath)
+            h, w, _ = self.image.shape
+            # Ubah ukuran jendela sesuai dengan ukuran gambar
+            self.resize(w ,h) # Penyesuaian dilakukan untuk menampung dua gambar dan beberapa padding
+            self.show_image(self.image, 'before')
+
+    def save_image(self):
+        filepath, _ = QFileDialog.getSaveFileName(self, "Save Image")
+        if filepath:
+            cv2.imwrite(filepath, self.processed_image)
+
+    def show_image(self, image, pos='before'):
+        h, w, ch = image.shape
+        bytes_per_line = ch * w
+        image = QImage(image.data, w, h, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap.fromImage(image)
+
+        if pos == 'before':
+            self.beforeImageView.setPixmap(pixmap)
+        else:
+            self.afterImageView.setPixmap(pixmap)
+
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Ui_MainWindow()
+    window.show()
+    sys.exit(app.exec_())
